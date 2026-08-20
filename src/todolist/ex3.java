741010c2823025028742dcb9695efd90d79c6ac8
jose <jose.v.teixeira6@aluno.senai.br>
2026-08-20 09:21:27 -0300

    alteraçoes aula 20/08

--- src/todolist/ex3.java
+++ src/todolist/ex3.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
  * @author Aluno
  */
 public class ex3 {
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
         String[] tarefas = new String[10];
@@ -25,7 +25,7 @@ public class ex3 {
          System.out.println("2 - Listar tarefas");
          System.out.println("3 - concluir tarefas");
          System.out.println("4 - excluir tarefa");
-         System.out.println("5 - excluir");
+         System.out.println("5 - sair");
          
          System.out.println("escolha uma opcao:");
          opcao = scanner.nextInt();
@@ -60,16 +60,14 @@ public class ex3 {
                  boolean existeTarefas = false;
                  
                  for(int i = 0; i < tarefas.length; i++){
-                     if(tarefas[i] != null){
-                         existeTarefas = true;
-                         
-                         if(concluidas[i] == true){
-                              System.out.println(("i + 1") + "= [x]" + tarefas [i] );
-                              
-                              
-                             
-                         }
-                     }
+                    if(tarefas[i] != null){
+                        existeTarefas = true;
+                        if(concluidas[i] == true){
+                             System.out.println((i + 1) + " = [x]" + tarefas [i] );
+                        }else{
+                            System.out.println((i + 1) + " = []" + tarefas [i] );
+                        }
+                    }
                  }
                  
                      if (existeTarefas == false){
@@ -93,6 +91,34 @@ public class ex3 {
                  }else{
                      System.out.println("tarefa invalida");
                  }
+             case 4:
+                 System.out.println("Digite o numero que deseja: ");
+                 int numeroExcluir = scanner.nextInt();
+                 
+                 int indiceExcluir = numeroExcluir -1;
+                 
+                 if (indiceExcluir >= 0 &&
+                         indiceExcluir < tarefas.length &&
+                         tarefas [indiceExcluir] != null){
+                     
+                    tarefas[indiceExcluir] = null;
+                    concluidas [indiceExcluir] = false;
+                    
+                     System.out.println("tarefa excluida com sucesso!");
+                     
+                 }else{
+                     System.out.println("tarefa invalida!");
+                 }
+                 
+                 break;
+             case 5:
+                 System.out.println("programa encerrado. ate mais!");
+                 
+                 break;
+             default:
+                 System.out.println("opcao invalida!");
+                 
+                 break;
              
                          
                          
