e9876b63fed52add71ae4316cd116df6b4fd2b38
jose <jose.v.teixeira6@aluno.senai.br>
2026-09-23 11:10:27 -0300

    alteraçoes 23\09

--- src/todolist/TelasToDoList.java
+++ src/todolist/TelasToDoList.java
@@ -4,6 +4,10 @@
  */
 package todolist;
 
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author Aluno
@@ -11,12 +15,20 @@ package todolist;
 public class TelasToDoList extends javax.swing.JFrame {
     
     private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(TelasToDoList.class.getName());
+    DefaultTableModel model;
+    
+    private static final String CONCLUIDA = "Concluir";
+     private static final String NAO_CONCLUIDA = "Não Concluida";
+     
+     private final ArrayList<String> tarefas = new ArrayList<>();
+     private final ArrayList<String> tarefasFiltradas = new ArrayList<>();
 
-    /**
-     * Creates new form TelasToDoList
-     */
     public TelasToDoList() {
         initComponents();
+        
+        setLocationRelativeTo(null);
+        
+        model = (DefaultTableModel) jTableTarefas.getModel();
     }
 
     /**
@@ -117,9 +129,57 @@ public class TelasToDoList extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButtonAdicionarTarefaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonAdicionarTarefaActionPerformed
-        // TODO add your handling code here:
+        if (jTextFieldDescricaoTarefa.getText().trim().isEmpty()){
+            JOptionPane.showMessageDialog(null, "A descriçao da tarefa nao pode ser vazia");
+            return;
+        }
+        
+        if (hasTarefaRepetida(jTextFieldDescricaoTarefa.getText())){
+            JOptionPane.showMessageDialog(null," A tarefa" + jTextFieldDescricaoTarefa.getText()+ "já existe!");
+            return;
+        }
+        
+        tarefas.add(jTextFieldDescricaoTarefa.getText() + ";" + NAO_CONCLUIDA);
+        
+        preencherTabela();
+        
+        jTextFieldDescricaoTarefa.setText("");
     }//GEN-LAST:event_jButtonAdicionarTarefaActionPerformed
 
+    
+    public boolean hasTarefaRepetida(String novaTarefa){
+        for (String tarefa : tarefas){
+            String dados [] = tarefa.split(";");
+            
+            if (novaTarefa.toLowerCase().equals(dados[0].toLowerCase())){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    private void preencherTabela(){
+        ArrayList<String> listaTarefas;
+        
+        if(jComboBoxFiltroStatus.getSelectedIndex() > 0){
+            listaTarefas = tarefasFiltradas;
+        }else{
+            listaTarefas = tarefas;
+        }
+        
+        model.setRowCount(0);
+        
+        for (String tarefa : listaTarefas){
+            String[] dados = tarefa.split((";"));
+            
+            model.addRow(new Object[]{
+                dados[0],
+                dados [1]
+                    
+                
+            });
+        }
+    }
     /**
      * @param args the command line arguments
      */
@@ -154,4 +214,10 @@ public class TelasToDoList extends javax.swing.JFrame {
     private javax.swing.JTable jTableTarefas;
     private javax.swing.JTextField jTextFieldDescricaoTarefa;
     // End of variables declaration//GEN-END:variables
+
+    private static class CONCLUIDA {
+
+        public CONCLUIDA() {
+        }
+    }
 }
